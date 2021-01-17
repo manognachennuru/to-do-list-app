bb8790239d14ec7d52e4ea4193dc7529026df4b5
manognachennuru <manognachennuru@gmail.com>
2021-01-17 22:29:11 +0530

    todo app using vanilla javascript

---
+++ app.js
@@ -0,0 +1,29 @@
+const input = document.getElementById('input');
+const form = document.getElementById('form');
+const todos = document.getElementById('todos');
+
+form.addEventListener('submit' , (e) => {
+    e.preventDefault();
+
+    const todoText = input.value;
+    if(todoText) {
+        const todoEl = document.createElement('li');
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+        })
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            todoEl.remove();
+        })
+
+        todos.appendChild(todoEl);
+
+    }
+
+    input.value = '';
+
+
+})
